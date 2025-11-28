fb2260e3faa9b7f530f536d8f2934be6333a73da
Arina <arina.klushnik@gmail.com>
2025-11-29 03:08:46 +0500

    MessageUser

---
+++ src/main/java/User.java
@@ -0,0 +1,24 @@
+public class User {
+
+    final String name;
+    final String id;
+
+    public User(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return ("Имя: " + getName() + ", ID: " + getId());
+
+    }
+}
