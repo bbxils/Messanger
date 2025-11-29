72c9b8677ea62a0cf52e668ed6b2cc318d80248c
Arina <arina.klushnik@gmail.com>
2025-11-29 10:55:18 +0500

    create Message

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,2 +1,22 @@
+import java.time.format.DateTimeFormatter;
+
 public class Message {
+    private User toUser;
+    private User fromUser;
+    private String text;
+    private String date;
+
+    public Message(User fromUser, User toUser, String text) {
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "от: " + fromUser.getName() +
+                "кому: " + toUser.getName() +
+                "текст: " + text +
+                "дата" + date;
+    }
 }
