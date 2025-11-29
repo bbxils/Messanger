0271c2bae15f865c621085bb807c61af4b194a99
Amina <maminan007@gmail.com>
2025-11-29 03:03:56 +0300

    создание группы, доб и уд участников,массив новых участников

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -1,2 +1,45 @@
 public class Group {
+
+    // Поля класса
+    private int id;
+    private String name;
+    private User[] members;
+    private int memberCount;
+
+    public Group(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.members = new User[10];
+        this.memberCount = 0; // сначала никого нет
+    }
+
+    public void addMember(User user) {
+        int number = members.length;
+        if (memberCount < number) {
+            members[memberCount] = user;
+            memberCount++;
+        } else {
+            System.out.println("группа полнаяя");
+        }
+    }
+
+    public void removeMember(User user) {
+        for (int i = 0; i < memberCount; i++) {
+            if (members[i] == user) {
+                members[i] = members[memberCount - 1]; //последний теперь присоединившийся
+                members[memberCount - 1] = null;
+                memberCount--; //
+                break; //
+            }
+        }
+    }
+
+    //новый массив
+    public User[] getMembers() {
+        User[] currentMembers = new User[memberCount];
+        for (int i = 0; i < memberCount; i++) {
+            currentMembers[i] = members[i];
+        }
+        return currentMembers;
+    }
 }
